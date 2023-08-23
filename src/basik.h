33cc6f172b9a5d8e58978db401c39d4c2d2f6dca
ChickChicky <chickin@evan-pc>
2023-08-23 20:42:01 +0200

    Big Update * Created a header file * Fixed Stack * Fixed dynvar_set * Fixed GC * Fixed big memory leaks

---
+++ src/basik.h
@@ -0,0 +1,169 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <cinttypes>
+
+using namespace std;
+
+// Forward-Decls //
+
+struct const_data_t;
+struct basik_val;
+struct basik_var;
+
+struct BasikChar;
+struct BasikI16;
+struct BasikI32;
+struct BasikI64;
+struct BasikString;
+struct BasikList;
+
+struct BasikException;
+struct Result;
+
+template<typename T> struct Stack;
+struct Buffer;
+
+// Simple Data //
+
+enum OpCodes : uint16_t {
+    End,
+    StoreSimple,
+    LoadSimple,
+    StoreDynamic,
+    LoadDynamic,
+    PushString,
+    PushChar,
+    PushI16,
+    PushI32,
+    PushI64,
+    ListBegin,
+    ListEnd,
+    ListExpand,
+    RemoveDynamic,
+    Add,
+    Sub,
+    Div,
+    Mul,
+    Pop,
+    Dup,
+    Jump,
+    JumpIf
+};
+
+enum DataType : uint16_t {
+    String,
+    Char,
+    I16,
+    I32,
+    I64,
+    List
+};
+
+const char* DataTypeStr[] = {
+    "String",
+    "Char",
+    "I16",
+    "I32",
+    "I64",
+    "List",
+};
+
+constexpr inline const char* get_data_type_str(DataType dt);
+
+// Basic Structures //
+
+struct const_data_t {
+    uint64_t sz;
+    void* data;
+};
+
+struct basik_val {
+    DataType type;
+    void* data;
+    ~basik_val();
+};
+
+struct basik_var {
+    basik_val* data;
+    const char* name;
+};
+
+// Type Data Structures //
+
+struct BasikChar  {
+    uint8_t* data;
+
+    BasikChar(uint8_t data);
+    ~BasikChar();
+};
+
+struct BasikI16  {
+    int16_t* data;
+
+    BasikI16(int16_t data);
+    ~BasikI16();
+};
+
+struct BasikI32  {
+    int32_t* data;
+
+    BasikI32(int32_t data);
+    ~BasikI32();
+};
+
+struct BasikI64 {
+    int64_t* data;
+
+    BasikI64(int64_t data);
+    ~BasikI64();
+};
+
+struct BasikString {
+    uint8_t* data;
+    size_t len;
+
+    BasikString(size_t len, const char* data);
+    ~BasikString();
+};
+
+struct BasikList {
+    Stack<basik_val>* data;
+
+    BasikList();
+    ~BasikList();
+
+    void append(basik_val v);
+
+    basik_val inline const operator[](size_t i);
+};
+
+
+struct BasikException {
+    const char* text;
+    int64_t code;
+};
+
+
+// Data Structures // 
+
+struct Result {
+    BasikException* except;
+    basik_val* value;
+};
+
+struct Buffer {
+
+    size_t    size;
+    uint8_t*  data;
+    size_t   _size;
+    uint8_t* _data;
+
+    Buffer(size_t size);
+    Buffer(size_t size, void* data);
+
+    ~Buffer();
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,21 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <cinttypes>
+#include "basik.h"
 
-using namespace std;
+/********************************\ 
+* Implementations for the header *
+\********************************/
 
-struct Buffer {
+/**
+ * Retreives the string representation of a data type value
+ */
+constexpr inline const char* get_data_type_str(DataType dt) {
+    if (dt > sizeof(DataTypeStr)/sizeof(const char*)) return "<INVALID TYPE>";
+    return DataTypeStr[dt];
+}
 
-    size_t    size;
-    uint8_t*  data;
-    size_t   _size;
-    uint8_t* _data;
-
-    Buffer(size_t size) {
-        this->data = this->_data = (uint8_t*)malloc(size);
-        this->size = this->_size = size;
-    }
-
-    Buffer(size_t size, void* data) {
-        this->data = this->_data = (uint8_t*)malloc(size);
-        this->size = this->_size = size;
-        memcpy(this->data,data,size);
-    }
-
-    ~Buffer() {
-        free(this->_data);
-    }
-
-};
-
+/**
+ * A structure to store data with an access towards the top and with configurable growth
+ * NOTE: It will probably be put in the header at some point, but for now I'm a little too lazy because of the template thing
+ */
 template<typename T>
 struct Stack {
     T** data;
@@ -54,17 +41,32 @@ struct Stack {
         this->size = 0;
     }
     void grow(size_t size) {
-        if (this->cap <= size) return;
+        if (this->cap > size) return;
         T** new_data = new T*[size];
-        memcpy(new_data,this->data,this->size);
-        // Destroy all of the ghost data
+        memcpy(new_data,this->data,this->size*sizeof(T*));
+        // Destroy all of the ghost data at the top of the stack
         for (size_t i = this->size; i < this->cap; i++) if (this->data[i] != nullptr) delete this->data[i];
+        delete[] this->data;
+        printf("STAC %zu %zu\n",this->size,this->cap);
         this->data = new_data;
         this->cap = size;
+        printf("STAK %zu %zu\n",this->size,this->cap);
     }
     void push(T* value) {
-        if (this->size == this->cap-1) this->grow(this->size+this->cap_grow);
+        if (this->size >= this->cap-1) this->grow(this->size+this->cap_grow);
         this->data[this->size++] = value;
+    }
+    void prune() {
+        T** tmp_data = new T*[this->size];
+        size_t sz = 0;
+        for (size_t i = this->size; i < this->cap; i++) if (this->data[i] != nullptr) delete this->data[i];
+        for (size_t i = 0; i < this->size; i++)         if (this->data[i] != nullptr) tmp_data[sz++] = this->data[i];
+        delete[] this->data;
+        this->data = new T*[sz];
+        memcpy(this->data,tmp_data,sz*sizeof(T*));
+        delete[] tmp_data;
+        this->size = sz;
+        this->cap = sz;
     }
     T* pop() {
         if (this->size == 0) return nullptr;
@@ -75,187 +77,168 @@ struct Stack {
     }
 };
 
-enum OpCodes : uint16_t {
-    End,
-    StoreSimple,
-    LoadSimple,
-    StoreDynamic,
-    LoadDynamic,
-    PushString,
-    PushChar,
-    PushI16,
-    PushI32,
-    PushI64,
-    ListBegin,
-    ListEnd,
-    ListExpand,
-    RemoveDynamic,
-    Add,
-    Sub,
-    Div,
-    Mul
-};
+// Basic Data Structures //
 
-enum DataType : uint16_t {
-    String,
-    Char,
-    I16,
-    I32,
-    I64,
-    List
-};
-
-const char* DataTypeStr[] = {
-    "String",
-    "Char",
-    "I16",
-    "I32",
-    "I64",
-    "List",
-};
-
-constexpr inline const char* get_data_type_str(DataType dt) {
-    if (dt > sizeof(DataTypeStr)/sizeof(const char*)) return "<INVALID TYPE>";
-    return DataTypeStr[dt];
+basik_val::~basik_val() {
+         if (this->type == DataType::Char)   delete ((BasikChar*)  this->data);
+    else if (this->type == DataType::I16)    delete ((BasikI16*)   this->data);
+    else if (this->type == DataType::I32)    delete ((BasikI32*)   this->data);
+    else if (this->type == DataType::I64)    delete ((BasikI64*)   this->data);
+    else if (this->type == DataType::String) delete ((BasikString*)this->data);
+    else if (this->type == DataType::List)   delete ((BasikList*)  this->data);
+    else {
+        fprintf(stderr,"Unkown data type for free\n");
+        exit(1);
+    }
 }
 
-struct const_data_t {
-    uint64_t sz;
-    void* data;
-};
+// Types Data Structures //
 
-struct basik_val {
-    DataType type;
-    void* data;
-};
+// Char
+BasikChar::BasikChar(uint8_t data) {
+    this->data = new uint8_t(data);
+}
+BasikChar::~BasikChar() {
+    delete this->data;
+}
 
-struct basik_var {
-    basik_val* data;
-    const char* name;
-};
+// I16
+BasikI16::BasikI16(int16_t data) {
+    this->data = new int16_t(data);
+}
+BasikI16::~BasikI16() {
+    delete this->data;
+}
 
-struct BasikString {
-    uint8_t* data;
-    size_t len;
-    BasikString(size_t len, const char* data) {
-        this->data = new uint8_t[len];
-        this->len = len;
-        memcpy(this->data,data,len);
-    }
-    ~BasikString() {
-        delete[] this->data;
-        this->len = 0;
-    }
-};
+// I32
+BasikI32::BasikI32(int32_t data) {
+    this->data = new int32_t(data);
+}
+BasikI32::~BasikI32() {
+    delete this->data;
+}
 
-struct BasikChar  {
-    uint8_t* data;
-    BasikChar(uint8_t data) {
-        this->data = new uint8_t(data);
-    }
-    ~BasikChar() {
-        delete this->data;
-    }
-};
+// I64
+BasikI64::BasikI64(int64_t data) {
+    this->data = new int64_t(data);
+}
+BasikI64::~BasikI64() {
+    delete this->data;
+}
 
-struct BasikI16  {
-    uint16_t* data;
-    BasikI16(uint16_t data) {
-        this->data = new uint16_t(data);
-    }
-    ~BasikI16() {
-        delete this->data;
-    }
-};
+// String
+BasikString::BasikString(size_t len, const char* data) {
+    this->data = new uint8_t[len];
+    this->len = len;
+    memcpy(this->data,data,len);
+}
+BasikString::~BasikString() {
+    delete[] this->data;
+    this->len = 0;
+}
 
-struct BasikI32  {
-    uint32_t* data;
-    BasikI32(uint32_t data) {
-        this->data = new uint32_t(data);
-    }
-    ~BasikI32() {
-        delete this->data;
-    }
-};
+// List
+BasikList::BasikList() {
+    this->data = new Stack<basik_val>();
+}
+void BasikList::append(basik_val v) {
+    this->data->push(new basik_val{v.type,v.data});
+}
+basik_val inline const BasikList::operator[](size_t i) {
+    return *this->data->data[i];
+}
+BasikList::~BasikList() {
+    delete this->data;
+}
 
-struct BasikI64 {
-    uint64_t* data;
-    BasikI64(uint64_t data) {
-        this->data = new uint64_t(data);
-    }
-    ~BasikI64() {
-        delete this->data;
-    }
-};
+// Other Data Structures //
 
-struct BasikList {
-    Stack<basik_val>* data;
-    BasikList() {
-        this->data = new Stack<basik_val>();
-    }
-    void append(basik_val v) {
-        this->data->push(new basik_val{v.type,v.data});
-    }
-    basik_val inline const operator[](size_t i) {
-        return *this->data->data[i];
-    }
-    ~BasikList() {
-        delete this->data;
-    }
-};
+Buffer::Buffer(size_t size) {
+    this->data = this->_data = (uint8_t*)malloc(size);
+    this->size = this->_size = size;
+}
 
-struct BasikException {
-    const char* text;
-    int64_t code;
-};
+Buffer::Buffer(size_t size, void* data) {
+    this->data = this->_data = (uint8_t*)malloc(size);
+    this->size = this->_size = size;
+    memcpy(this->data,data,size);
+}
 
-struct Result {
-    BasikException* except;
-    basik_val* value;
-};
+Buffer::~Buffer() {
+    free(this->_data);
+}
 
+/********************************\ 
+*           Main stuff           *
+\********************************/
+
+/**
+ * A structure allowing to store references to stuff and delete them when they're no longer used
+ */
 struct gc_t {
 
     struct gc_ref {
-        void* v;
+        basik_val* v;
         size_t c;  
     };
 
     Stack<gc_ref> refs = Stack<gc_ref>(256,256);
 
-    bool add_ref( void* v ) {
+    /**
+     * Adds a reference to a pointer, giving back the amount of references to the pointer after the operation
+     * Returns whether a new entry was created
+     */
+    inline bool add_ref_ex( basik_val* v, size_t* count ) {
         for (size_t i = 0; i < refs.size; i++) {
             gc_ref* r = refs.data[i];
             if (r != nullptr && refs.data[i]->v == v) {
                 r->c++;
-                //printf("\t\t\t\tGC STR %p %zu\n",v,r->c);
+                printf("\t\t\t\tGC ADD %p %zu\n",v,r->c);
+                if (count != nullptr) *count = r->c;
                 return false;
             }
         }
-        //printf("\t\t\t\tGC NEW %p\n",v);
+        printf("\t\t\t\tGC NEW %p 1\n",v);
+        if (count != nullptr) *count = 1;
         refs.push(new gc_ref{v,1});
         return true;
     }
 
-    bool remove_ref_ex( void* v, size_t* count ) {
-        gc_ref* r = nullptr;
-        for (size_t i = 0; i < refs.size; i++) {
-            if (refs.data[i] != nullptr && refs.data[i]->v == v) {
-                r = refs.data[i];
-                break;
-            }
-        }
-        if (r == nullptr) return false;
-        if (r->c) r->c--;
-        //printf("\t\t\t\tGC REM %p %zu\n",r->v,r->c);
-        if (count != nullptr) {
-            *count = r->c;
-        }
-        return true;
+    /**
+     * Adds a reference to a pointer
+     */ 
+    inline bool add_ref( basik_val* v ) {
+        return add_ref_ex(v,nullptr);
     }
 
-    inline bool remove_ref( void* v ) {
-        return this->remove_ref_ex(v,nullptr);
+    /**
+     * Removes a reference to a pointer, giving back the amount of references to the pointer after the operation
+     * Returns whether the reference was found
+     */
+    bool remove_ref( basik_val* v ) {
+        for (size_t i = 0; i < refs.size; i++) {
+            if (refs.data[i] != nullptr && refs.data[i]->v == v) {
+                if(refs.data[i]->c)--refs.data[i]->c;
+                printf("\t\t\t\tGC REM %p %zu\n",refs.data[i]->v,refs.data[i]->c);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    inline size_t collect( void ) {
+        size_t c = 0;
+        for (size_t i = 0; i < this->refs.size; i++) {
+            gc_ref* r = this->refs.data[i];
+            if (r != nullptr && r->c == 0) {
+                printf("\t\t\t\tGC COL %p\n",r->v);
+                delete r->v;
+                delete r;
+                c++;
+            }
+        }
+        this->refs.prune();
+        return c;
     }
 
 };
@@ -270,6 +253,7 @@ struct Env {
     const_data_t** const_data;
     const char* bytecode;
     uint8_t* ptr;
+    uint8_t* orig;
     gc_t gc;
 
     Env() {
@@ -282,6 +266,13 @@ struct Env {
      */
     void dynvar_set(const char* name, basik_val* value) {
         gc.add_ref(value);
+        for (size_t i = 0; i < dynamic_vars.size; i++) {
+            basik_var* vv = dynamic_vars.data[i];
+            if (vv != nullptr && !strcmp(vv->name,name)) {
+                vv->data = value;
+                return;
+            }
+        }
         basik_var* v = new basik_var{value,name};
         // Tries to find an empty space in the variables
         for (size_t i = 0; i < dynamic_vars.size; i++) {
@@ -320,9 +311,9 @@ struct Env {
         return false;
     }
 
-    bool stack_push(basik_val* v) {
-        gc.add_ref(v);
+    inline bool stack_push(basik_val* v) {
         if (stacki >= sizeof(stack)) return false;
+        gc.add_ref(v);
         stack[stacki++] = v;
         return true;
     }
@@ -372,6 +363,8 @@ void pre_run(Env* env) {
         ptr += l+1;
     }
 
+    env->orig = ptr;
+
 }
 
 Result run(Env* env) {
@@ -392,20 +385,21 @@ Result run(Env* env) {
     // Running
 
     uint8_t* prog = ptr;
-    uint8_t* orig = ptr;
 
     while (*(uint16_t*)prog) {
         uint16_t op = *(uint16_t*)prog;
         prog += 2;
 
+        printf("----- %d %zu -----\n",op,stacki);
+
         if (op == OpCodes::End) {
             /* do nothing */
             /* this should not even ever happen */
-        } else
+        }
 
         // Store
 
-        if (op == OpCodes::StoreSimple) {
+        else if (op == OpCodes::StoreSimple) {
             basik_val* val = env->stack_pop();
             uint32_t var = *(uint32_t*)prog; prog += 4;
             if (val == nullptr) {
@@ -415,22 +409,27 @@ Result run(Env* env) {
             if (simple_vars[var]->data != nullptr) gc.remove_ref(simple_vars[var]->data);
             gc.add_ref(val);
             simple_vars[var]->data = val;
-        } else
+        }
 
-        if (op == OpCodes::StoreDynamic) {
+        else if (op == OpCodes::StoreDynamic) {
             basik_val* val = env->stack_pop();
             if (val == nullptr) {
                 fprintf(stderr,"Got NULL for StoreDynamic\n");
                 exit(1);
             }
             const char* varname = (const char*)prog; prog += strlen((const char*)prog)+1;
+            basik_val* var = env->dynvar_get(varname);
+            if (var != nullptr) {
+                printf("\t\t\t\tR %p\n",var);
+                gc.remove_ref(var);
+            }
             printf("STOR DYN %s = %d\n",varname,*((BasikI32*)val->data)->data);
             env->dynvar_set(varname,val);
-        } else
+        }
 
         // Load
 
-        if (op == OpCodes::LoadSimple) {
+        else if (op == OpCodes::LoadSimple) {
             uint32_t var = *(uint32_t*)prog; prog += 4;
             basik_val* val = simple_vars[var]->data;
             if (val == nullptr) {
@@ -438,9 +437,9 @@ Result run(Env* env) {
                 exit(1);
             }
             env->stack_push(val);
-        } else
+        }
 
-        if (op == OpCodes::LoadDynamic) {
+        else if (op == OpCodes::LoadDynamic) {
             const char* varname = (const char*)prog; prog += strlen((const char*)prog)+1;
             basik_val* val = env->dynvar_get(varname);
             if (val == nullptr) {
@@ -449,53 +448,53 @@ Result run(Env* env) {
             }
             printf("LOAD DYN %s = %d\n",varname,*((BasikI32*)val->data)->data);
             env->stack_push(val);
-        } else
+        }
 
         // Remove
 
-        if (op == OpCodes::RemoveDynamic) { // Cleans up a dynamic variable
+        else if (op == OpCodes::RemoveDynamic) { // Cleans up a dynamic variable
             const char* varname = (const char*)prog; prog += strlen((const char*)prog)+1;
             env->dynvar_rem(varname);
-        } else
+        }
 
         // Integers
 
-        if (op == OpCodes::PushChar) {
+        else if (op == OpCodes::PushChar) {
             uint8_t data = *(uint8_t*)prog;
             env->stack_push(new basik_val{DataType::Char,new BasikChar(data)});
             prog += 1;
-        } else
-        if (op == OpCodes::PushI16) {
+        }
+        else if (op == OpCodes::PushI16) {
             int16_t data = *(int16_t*)prog;
             env->stack_push(new basik_val{DataType::I16,new BasikI16(data)});
             prog += 2;
-        } else
-        if (op == OpCodes::PushI32) {
+        }
+        else if (op == OpCodes::PushI32) {
             int32_t data = *(int32_t*)prog;
             env->stack_push(new basik_val{DataType::I32,new BasikI32(data)});
             prog += 4;
-        } else
-        if (op == OpCodes::PushI64) {
+        }
+        else if (op == OpCodes::PushI64) {
             int64_t data = *(int64_t*)prog;
             env->stack_push(new basik_val{DataType::I64,new BasikI64(data)});
             prog += 8;
-        } else
+        }
 
         // String
 
-        if (op == OpCodes::PushString) {
+        else if (op == OpCodes::PushString) {
             int32_t val_addr = *(int32_t*)prog;
             const_data_t* data = const_data[val_addr];
             env->stack_push(new basik_val{DataType::String,new BasikString(data->sz,(const char*)data->data)});
             prog += 4;
-        } else
+        }
 
         // List
 
-        if (op == OpCodes::ListBegin) {
+        else if (op == OpCodes::ListBegin) {
             list_stack.push(new size_t(stacki));
-        } else
-        if (op == OpCodes::ListEnd) {
+        }
+        else if (op == OpCodes::ListEnd) {
             if (list_stack.size == 0) {
                 fprintf(stderr,"Attempt to close a list that was not open\n");
                 exit(1);
@@ -508,8 +507,8 @@ Result run(Env* env) {
             }
             for (size_t i = 0; i < list_size; i++) env->stack_pop();
             env->stack_push(new basik_val{DataType::List,list});
-        } else
-        if (op == OpCodes::ListExpand) {
+        }
+        else if (op == OpCodes::ListExpand) {
             basik_val* val = env->stack_pop();
             if (val == nullptr) {
                 fprintf(stderr,"Attempt to expand NULL\n");
@@ -525,11 +524,11 @@ Result run(Env* env) {
                 fprintf(stderr,"Expand does not support type `%s`\n",get_data_type_str(val->type));
                 exit(1);
             }
-        } else
+        }
 
         // Arithmetic
 
-        if (op == OpCodes::Add) {
+        else if (op == OpCodes::Add) {
             basik_val* a = env->stack_pop();
             basik_val* b = env->stack_pop();
             if (a == nullptr || b == nullptr) {
@@ -566,11 +565,37 @@ Result run(Env* env) {
             }
         }
 
+        // Stack
+
+        else if (op == OpCodes::Pop) {
+            env->stack_pop();
+        }
+
+        else if (op == OpCodes::Dup) {
+            basik_val* v = env->stack_pop();
+            env->stack_push(v);
+            env->stack_push(v);
+        }
+
+        // Jumping
+
+        else if (op == OpCodes::Jump) {
+            uint64_t addr = *(uint64_t*)prog;
+            prog += 8;
+            prog = env->orig + addr;
+        }
+
+        // ???
+
         else {
             fprintf(stderr,"Unknown instruction opcode: `%u`\n",op);
             exit(1);
         }
+    
+        gc.collect();
+
     }
+
 
     return Result{nullptr,nullptr};
 
@@ -590,29 +615,12 @@ int main() {
         "\x08\x00"             // Push I32
             "\x02\x00\x00\x00" // 1
 
-        "\x08\x00"             // Push I32
-            "\x01\x00\x00\x00" // 2
-        
-        "\x0E\x00"             // Add
-
-        "\x01\x00"             // Store simple
-            "\x00\x00\x00\x00" // 0 (x)
-
-        "\x02\x00"
-            "\x00\x00\x00\x00"
-
         "\x03\x00"             // Store Dynamic
             "z\0"              // "z"
 
-        "\x04\x00"             // Load Dynamic
-            "z\0"              // "z"
+        "\x14\x00"
+            "\x00\x00\x00\x00\x00\x00\x00\x00"
 
-        "\x0D\x00"             // Remove Dynamic
-            "z\0"
-
-        "\x04\x00"             // Load Dynamic
-            "z\0"              // "z"
-        
         "\x00\x00" // End of program
     ;
 
